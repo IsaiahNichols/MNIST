b5f2e7bf81a462636d933ffe6b023dece691cfb4
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-03-27 21:27:06 -0700

    finished body of main, just need to do a lot of debugging

--- main.py
+++ main.py
@@ -16,21 +16,21 @@ num_of_epochs = 10
 batch_size = 64
 
 # Loading Data
-transform = torchvision.transforms.Compose(torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,)))
+transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))])
 
 train_data = torchvision.datasets.MNIST("./data", train=True, transform=transform, download=True)
-train_data_loader = torch.utils.dataloader(train_data, batch_size=batch_size, shuffle=True)
+train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
 test_data = torchvision.datasets.MNIST("./data", train=False, transform=transform, download=True)
-test_data_loader = torch.utils.dataloader(test_data, batch_size=batch_size, shuffle=False)
+test_data_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
 
 # Creating Model
 class Net(torch.nn.Module):
     def __init__(self) -> None:
         super(Net, self).__init__()
-
-        self.conv1 = torch.nn.Conv2d(1, 32, kernal_size=5, padding=2)
-        self.conv2 = torch.nn.Conv2d(32, 64, kernal_size=5, padding=2)
+        self.conv1 = torch.nn.Conv2d(1, 32, kernel_size=5, padding=2)
+        self.conv2 = torch.nn.Conv2d(32, 64, kernel_size=5, padding=2)
         self.lin1 = torch.nn.Linear(5*5*64, 1024)
         self.lin2 = torch.nn.Linear(1024, 10)
         self.relu = torch.nn.ReLU()
@@ -51,4 +51,39 @@ class Net(torch.nn.Module):
         output = self.lin2(output)
         return output
 
-# Training Model
+
+model = Net()
+
+# Train Model
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_of_epochs):
+    for i, (images, labels) in enumerate(train_data_loader):
+        # Forward Pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward Propagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Output Relevant Data
+        print(f"Epoch: {epoch}/{num_of_epochs}; Step: {i}/{len(train_data_loader)}; Loss: {loss.item():.10f}")
+
+# Test Model
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+
+    for images, labels in test_data_loader:
+        output = model(images)
+
+        index, prediction = torch.max(output)
+
+        total += labels.size(0)
+        correct += (prediction == labels).sum().item()
+    
+    print(f"Accuracy of Model: {(correct/total)*100}%")
